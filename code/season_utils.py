76c266e59792ec8f72f633e26f984b2ee6066f8f
Mihin Methsara Prathapasinghe <mihinmethsaraprathapasinghe23@gmail.com>
2026-05-24 03:41:44 +0530

    add traditional season function

--- code/season_utils.py
+++ code/season_utils.py
@@ -91,3 +91,56 @@ def get_meteorological_season(country, month):
 
 
 
+def get_traditional_season(country, month):
+     #module 2 find traditional noongar season for australia from figure1
+
+
+    country = str(country).strip().title() 
+    
+     
+
+    if isinstance(month, (int, float)):
+        month_str = str(int(month)).strip()
+    else:
+        month_str = str(month).strip().title()
+
+
+    month_numbers = {
+        
+        "1": "January", "2": "February", "3": "March", "4": "April",
+        "5": "May", "6": "June", "7": "July", "8": "August",
+        "9": "September", "10": "October", "11": "November", "12": "December",
+         "01": "January", "02": "February", "03": "March", "04": "April",
+        "05": "May", "06": "June", "07": "July", "08": "August", "09": "September"
+    }
+
+    if month_str in month_numbers:
+       month_str = month_numbers[month_str]
+
+
+       #store noongar traditional seasons from figure1
+    noongar_seasons = {
+
+        "Birak": ["December", "January"],
+        "Bunuru": ["February", "March"],
+        "Djeran": ["April", "May"],
+        "Makuru": ["June", "July"],
+        "Djilba": ["August", "September"],
+         "Kambarang": ["October", "November" ]
+    }
+
+
+      #check if country is australia becauce  australia only have traditional seasons
+    if country != "Australia":
+        return "No traditional season for this country"
+    
+
+       #find matching season and return
+
+    for season_name, valid_months in noongar_seasons.items():
+        if month_str in valid_months:
+            return season_name
+
+
+     #if month not match any season
+    return "No traditional season for this month" 
