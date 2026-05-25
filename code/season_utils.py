48d6a976d1a4a18093091fddf8b3e21ad21329a8
Mihin Methsara Prathapasinghe <mihinmethsaraprathapasinghe23@gmail.com>
2026-05-25 18:12:02 +0530

    complete intial  meteorological and traditional season functions with validation

--- code/season_utils.py
+++ code/season_utils.py
@@ -153,7 +153,6 @@ def check_seasons_same(country1, country2, month):
     season1 = get_meteorological_season(country1, month)
     season2 = get_meteorological_season(country2, month)
 
-
      
     print(f"\n---Season Comparison Result---") #show comparison result screen
     print(f"{country1} in {month}: {season1}")
